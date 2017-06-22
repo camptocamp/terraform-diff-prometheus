445faec70deb2501c606ac330d5713ce1450b8ea
Julien Acroute <julien.acroute@camptocamp.com>
2017-06-22 11:07:24 +0200

    Check diff every hour

--- provider/docker-entrypoint.d/05-provider-loop.sh
+++ provider/docker-entrypoint.d/05-provider-loop.sh
@@ -15,6 +15,6 @@ while true; do
     terraform init > /dev/null
     diff_count=`terraform plan -no-color | python /terraform-plan-formatter/terraform-plan-formatter.py -q`
     echo "terraform_plan_diff_count $diff_count" > /data/metrics/index.html
-    sleep 300
+    sleep 3600
 done
 
